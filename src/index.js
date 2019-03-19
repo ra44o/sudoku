b2c2d58341a3359e9b2700a835e94b105ba4ce9c
Artsiom Rak <ra44elo@gmail.com>
2019-03-19 23:07:19 +0300

    feat: add implementation of the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+
+  for (let row = 0; row < 9; row += 1) {
+    for (let column = 0; column < 9; column += 1) {
+      if (matrix[row][column] == 0) {
+        for (let currentNumber = 1; currentNumber <= 9; currentNumber += 1){
+          if (checkPosition(row, column, currentNumber)) {
+            matrix[row][column] = currentNumber;
+            if (solveSudoku(matrix))  {
+              return matrix;
+            }
+            else {
+              matrix[row][column] = 0;
+            }
+          }
+        }
+        return false;
+      }
+    }
+  }
+
+  function checkRow(row, currentNumber) {
+    for (let r = 0; r < 9; r += 1) {
+      if (matrix[row][r] == currentNumber) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function checkColumn(column, currentNumber) {
+    for (let c = 0; c < 9; c += 1) {
+      if (matrix[c][column] == currentNumber) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function checkSquare(row, column, currentNumber) {
+    let rows = Math.floor(row / 3) * 3;
+    let columns = Math.floor(column / 3) * 3;
+    for (let r = 0; r < 3; r += 1) {
+      for (let c = 0; c < 3; c += 1) {
+        if (matrix[rows + r][columns + c] == currentNumber) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  function checkPosition(row, column, currentNumber) {
+    return (
+            checkRow(row, currentNumber) && 
+            checkColumn(column, currentNumber) && 
+            checkSquare(row, column, currentNumber)
+    );
+  }
+
+  return matrix;
 }
